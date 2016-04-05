00a46a94d66e1f6d249bd2246d5cabff69a29559
Jacky Boen <jacky.boen@bartleboglehegarty.com>
2016-04-05 14:25:24 +0800

    code style
    Signed-off-by: Jacky Boen <jacky.boen@bartleboglehegarty.com>

--- public/js/components/src/Login.js
+++ public/js/components/src/Login.js
@@ -6,22 +6,22 @@ import ReactDOM from 'react-dom';
 class Login extends React.Component {
     render() {
         return (
-            <div className="login-page">
-                <a href="index.html"><img className="logo" src="images/playpinoy_logo_w.png" /></a>
-                <div className="form">
-                    <form className="register-form">
-                        <input type="text" placeholder="name"/>
-                        <input type="password" placeholder="password"/>
-                        <input type="text" placeholder="email address"/>
+            <div className='login-page'>
+                <a href='index.html'><img className='logo' src='images/playpinoy_logo_w.png' /></a>
+                <div className='form'>
+                    <form className='register-form'>
+                        <input type='text' placeholder='name'/>
+                        <input type='password' placeholder='password'/>
+                        <input type='text' placeholder='email address'/>
                         <button>create</button>
-                        <p className="message">Already registered? <a href="#">Sign In</a></p>
+                        <p className='message'>Already registered? <a href='#'>Sign In</a></p>
                     </form>
-                    <form className="login-form">
+                    <form className='login-form'>
                         <h4>login</h4>
                         <button style={{ backgroundColor: '#3c5a99' }}>Facebook</button>
                         <button style={{ backgroundColor: '#2ca7e0' }}>Twitter</button>
                         <button style={{ backgroundColor: '#a77b5e' }}>Instagram</button>
-                        <p className="message">Not registered? <a href="#">Create an account</a></p>
+                        <p className='message'>Not registered? <a href='#'>Create an account</a></p>
                     </form>
                 </div>
             </div>
